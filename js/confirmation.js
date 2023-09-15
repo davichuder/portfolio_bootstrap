78da4eceb1b0fe3059ee1efcf8394d05de8b2809
David Rolón <rolondavid@outlook.com>
2023-09-15 17:03:03 -0300

    style: Correcciones de estilos, y finalizacion de contacto
    Se crearon script de js para el contacto
    
    Falta ultimos retoques de estilo

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,21 +1,31 @@
-var confirmation_num1 = Math.floor(Math.random() * 90) + 10;
-var confirmation_num2 = Math.floor(Math.random() * 9) + 1;
+var digits_num1 = 2;
+var digits_num2 = 1;
+var confirmation_num1 = Math.floor(Math.random() * (9 * 10 ** (digits_num1 - 1))) + 10 ** (digits_num1 - 1);
+var confirmation_num2 = Math.floor(Math.random() * (9 * 10 ** (digits_num2 - 1))) + 10 ** (digits_num2 - 1);
 var confirmation_sum = confirmation_num1 + confirmation_num2;
+var text_confirmation = "¿Eres un robot?<br>¿Cual es la suma entre " + confirmation_num1 + " y " + confirmation_num2 + "?";
 
 $(function () {
-    $(".confirmation").html("¿Cual es la suma entre " + confirmation_num1 + " y " + confirmation_num2 + "?");
-});
 
-$(function () {
-    $('.contacto form button').on('submit', function () {
-        input_confirmation = parseInt(document.getElementById("sum").value);
-        if (input_confirmation === confirmation_sum) {
-            return false;
+    $(".confirmation").html(text_confirmation);
+    $('#sum').on('keyup', function () {
+        input_confirmation = document.getElementById("sum").value;
+        if (isNaN(input_confirmation)) {
+            let text_nan = '<strong style="color:yellow;">¡Ingrese un número!<strong>';
+            $(".confirmation").html(text_confirmation + " " + text_nan);
+            $("#submit-button").addClass("disabled");
+            return;
         }
-        $(".confirmation").html("¿Cual es la suma entre " + confirmation_num1 + " y " + confirmation_num2 + "? Responde correctamente");
-        $(".confirmation").css({"color": "red"});
-        $(".confirmation").transform({ "scale": "1.01" }, 100);
-        // .delay(2000).transform({ "scale": "0.99" }, 100).delay(100);
-        return false;
+        value_confirmation = parseInt(input_confirmation);
+        if (value_confirmation === confirmation_sum) {
+            let text_correcto = '<strong style="color:green;">¡Correcto!<strong>';
+            $(".confirmation").html(text_confirmation + " " + text_correcto);
+            $("#submit-button").removeClass("disabled");
+            return;
+        }
+        let text_incorrecto = '<strong style="color:red;">¡Incorrecto!<strong>';
+        $(".confirmation").html(text_confirmation + " " + text_incorrecto);
+        $("#submit-button").addClass("disabled");
+        console.log("false");
     });
 });
